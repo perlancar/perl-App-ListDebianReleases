ce814d9eadbc71d1de0f40c8c3b1c7fa93d56120
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-08-15 21:45:53 +0700

    Initial commit

---
+++ lib/App/ListDebianReleases.pm
@@ -0,0 +1,44 @@
+package App::ListDebianReleases;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Version::Util qw(cmp_version);
+
+our %SPEC;
+$SPEC{list_debian_releases} = {
+    v => 1.1,
+    summary => 'List Debian releases',
+    args => {
+        detail => {
+            schema => 'bool*',
+        },
+    },
+},
+sub list_debian_releases {
+    require Debian::Releases; # rather chubby
+
+    my $dr = Debian::Releases->new;
+    my $rel = $dr->releases;
+    #use Data::Dump; dd $rel;
+    my @res;
+    for (sort {cmp_version($a, $b)} keys $rel) {
+        push @res, $arg{detail} ? {a=>$_, b=>$rel->{$_}} : $_;
+    }
+    [200, "OK", \@res];
+}
+
+}
+
+1;
+# ABSTRACT: List Debian releases
+
+=head1 SEE ALSO
+
+L<Debian::Releases>
+
+=cut
