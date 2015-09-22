e61dad16f1693a5e20e1cd07239dd92aabeda47d
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-22 19:07:58 +0700

    Switch (temporarily?) to PERLANCAR::Debian::Releases

--- lib/App/ListDebianReleases.pm
+++ lib/App/ListDebianReleases.pm
@@ -3,43 +3,11 @@ package App::ListDebianReleases;
 # DATE
 # VERSION
 
-use 5.010001;
-use strict;
-use warnings;
-
-use Version::Util qw(cmp_version);
-
-our %SPEC;
-$SPEC{list_debian_releases} = {
-    v => 1.1,
-    summary => 'List Debian releases',
-    args => {
-        detail => {
-            schema => 'bool*',
-            cmdline_aliases => {l=>{}},
-        },
-    },
-};
-sub list_debian_releases {
-    require Debian::Releases; # rather chubby
-
-    my %args = @_;
-
-    my $dr = Debian::Releases->new;
-    my $rel = $dr->releases;
-    #use Data::Dump; dd $rel;
-    my @res;
-    for (sort {cmp_version($a, $b)} keys %$rel) {
-        push @res, $args{detail} ? {version=>$_, code_name=>$rel->{$_}} : $_;
-    }
-    [200, "OK", \@res];
-}
-
 1;
-# ABSTRACT:
+# ABSTRACT: List Debian releases (CLI)
 
-=head1 SEE ALSO
+=head1 DESCRIPTION
 
-L<Debian::Releases>
+See the included script L<list-debian-releases>.
 
 =cut
